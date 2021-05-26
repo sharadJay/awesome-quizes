1596778951679ba9a9196126093a2f6a32e75abb
Sharad Jain <sharadj1591@gmail.com>
2021-05-26 16:34:10 +0530

    Initial Commit

---
+++ Awesome Quizes/Awesome_QuizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Awesome_QuizesApp.swift
+//  Awesome Quizes
+//
+//  Created by Sharad Jain on 26/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Awesome_QuizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesome Quizes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Awesome Quizes
+//
+//  Created by Sharad Jain on 26/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
